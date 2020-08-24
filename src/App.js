ef7f849d873108642a6d2ff684bb3a0495dd903b
Kevin Vahdat <kevinvahdat35@gmail.com>
2020-08-24 18:29:21 -0400

    updated routing for menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = (props) => {
+  return (
+    <div>Hats Page</div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <Switch> */}
+        <Route exact path='/' component={HomePage}>
+        
+        </Route>
+        <Route path='/hats' component={HatsPage}>
+
+        </Route>
+      {/* </Switch> */}
+      {/* <HomePage /> */}
     </div>
   );
 }
